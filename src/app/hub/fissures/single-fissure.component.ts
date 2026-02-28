e794660a451c2a3736a82b097aa500c69ed5e5d1
Cory Evans <cory0x45@gmail.com>
2026-02-28 11:04:07 +0000

    fix: display omnia fissure icon

--- src/app/hub/fissures/single-fissure.component.ts
+++ src/app/hub/fissures/single-fissure.component.ts
@@ -1,19 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, computed, input, Input, OnInit } from '@angular/core';
 import { SettingsService } from 'src/app/shared/services/settings.service';
 
 @Component({
-    selector: 'fissure-card',
-    templateUrl: './single-fissure.component.html',
-    standalone: false
+  selector: 'fissure-card',
+  templateUrl: './single-fissure.component.html',
+  standalone: false,
 })
 export class SingleFissureComponent implements OnInit {
-  @Input() fissure?: WarframeApi.Fissure;
+  fissure = input.required<WarframeApi.Fissure>();
 
   constructor(private settingsService: SettingsService) {}
 
   ngOnInit(): void {}
 
-  get missionTypes() {
-    return this.settingsService.settings.highlightedFissures;
-  }
+  url = computed(() => {
+    const f = this.fissure();
+
+    let tier = f.tier.toLowerCase();
+
+    // omnia fissures are a mix of all the other fissures
+    if (tier === 'omnia') {
+      tier = 'lith';
+    }
+
+    return (
+      'https://raw.githubusercontent.com/WFCD/genesis-assets/master/svg/fissures/' +
+      tier +
+      '.svg'
+    );
+  });
+
+  highlighted = computed(() => {
+    return (
+      this.settingsService.settings.highlightedFissures.indexOf(
+        this.fissure().missionType,
+      ) >= 0
+    );
+  });
 }
